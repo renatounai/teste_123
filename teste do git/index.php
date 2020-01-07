2815d4943ee62c3246d1ccb1e95726bc9b87bdd3
Renato Custódio Pereira <renatomcn@gmail.com>
2020-01-07 12:02:26 -0300

    dsa

--- teste do git/index.php
+++ teste do git/index.php
@@ -2,11 +2,5 @@ um arquivo php / segunda alteração / terceira alteração / quinta alteração
 
 algo aqui
 
-<<<<<<< HEAD
 <?php echo 'eita' 
-=======
-<?php
 
-
-algo aqui embaixo
->>>>>>> branch 'master' of https://github.com/renatounai/teste_123.git
